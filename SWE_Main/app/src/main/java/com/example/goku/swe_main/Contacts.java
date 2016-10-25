9789476233f0d3381a37f31444a66c37daeb5d95
Goku <nishantgarg021294@gmail.com>
2016-10-25 09:46:41 +0530

    done the add message and add contacts

---
+++ SWE_Main/app/src/main/java/com/example/goku/swe_main/Contacts.java
@@ -0,0 +1,84 @@
+package com.example.goku.swe_main;
+
+/**
+ * Created by goku on 25/10/16.
+ */
+
+public class Contacts
+{
+    private String contact1;
+    private String person1;
+    private String contact2;
+    private String contact3;
+    private String contact4;
+    private String person2;
+    private String person3;
+    private String person4;
+
+    public Contacts() {
+    }
+
+    public String getContact1() {
+        return contact1;
+    }
+
+    public void setContact1(String contact1) {
+        this.contact1 = contact1;
+    }
+
+    public String getPerson1() {
+        return person1;
+    }
+
+    public void setPerson1(String person1) {
+        this.person1 = person1;
+    }
+
+    public String getContact2() {
+        return contact2;
+    }
+
+    public void setContact2(String contact2) {
+        this.contact2 = contact2;
+    }
+
+    public String getContact3() {
+        return contact3;
+    }
+
+    public void setContact3(String contact3) {
+        this.contact3 = contact3;
+    }
+
+    public String getContact4() {
+        return contact4;
+    }
+
+    public void setContact4(String contact4) {
+        this.contact4 = contact4;
+    }
+
+    public String getPerson2() {
+        return person2;
+    }
+
+    public void setPerson2(String person2) {
+        this.person2 = person2;
+    }
+
+    public String getPerson3() {
+        return person3;
+    }
+
+    public void setPerson3(String person3) {
+        this.person3 = person3;
+    }
+
+    public String getPerson4() {
+        return person4;
+    }
+
+    public void setPerson4(String person4) {
+        this.person4 = person4;
+    }
+}
